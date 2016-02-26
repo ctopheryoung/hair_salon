407b3b2cdb3d19fdc5827d868d126aa0749f5d92
Chris Young <ctopheryoung@gmail.com>
2016-02-26 11:58:40 -0800

    write passing root test for index.vtl

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,13 @@
 import org.fluentlenium.adapter.FluentTest;
+import static org.junit.Assert.*;
 import org.junit.*;
+import org.junit.ClassRule;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
-import static org.assertj.core.api.Assertions.*;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -20,6 +23,9 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-
-  //Tests go here
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Hair Salon Manager");
+  }
 }
