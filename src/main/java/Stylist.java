2debaad783236e5c0c3b2938c3a856fe11e3badb
Chris Young <ctopheryoung@gmail.com>
2016-02-26 11:19:36 -0800

    add all() method and test, equals() override and test

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -17,8 +17,26 @@ public class Stylist {
     return stylist;
   }
 
+  @Override
+  public boolean equals(Object otherStylist){
+    if (!(otherStylist instanceof Stylist)) {
+      return false;
+    } else {
+      Stylist newStylist = (Stylist) otherStylist;
+      return this.getStylist().equals(newStylist.getStylist()) &&
+        this.getId() == newStylist.getId();
+    }
+  }
+
   //CREATE
   //READ
+  //READ
+  public static List<Stylist> all() {
+    String sql = "SELECT id, stylist FROM stylists";
+    try (Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Stylist.class);
+    }
+  }
   //UPDATE
   //DELETE
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -1,12 +1,54 @@
 import org.junit.*;
 import static org.junit.Assert.*;
-import org.junit.Rule;
+import org.sql2o.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class StylistTest {
 
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
-  //Tests go here
+  @Test
+  public void all_emptyAtFirst() {
+      assertEquals(Stylist.all().size(), 0);
+  }
 
+  @Test
+  public void equals_returnsTrueIfStylistsAreTheSame() {
+    Stylist stylistOne = new Stylist("Bob");
+    Stylist stylistTwo = new Stylist("Bob");
+    assertTrue(stylistOne.equals(stylistTwo));
+  }
+
+  // @Test
+  // public void save_returnsTrueIfCuisinesAreTheSame() {
+  //   Cuisine testCuisine = new Cuisine("American");
+  //   testCuisine.save();
+  //   assertTrue(Cuisine.all().get(0).equals(testCuisine));
+  // }
+  //
+  // @Test
+  // public void save_assignsIdToObject() {
+  //   Cuisine myCuisine = new Cuisine("American");
+  //   myCuisine.save();
+  //   Cuisine savedCuisine = Cuisine.all().get(0);
+  //   assertEquals(myCuisine.getId(), savedCuisine.getId());
+  // }
+  //
+  // @Test
+  // public void update_updatesCuisineTypeInDB() {
+  //   Cuisine myCuisine = new Cuisine("American");
+  //   myCuisine.save();
+  //   myCuisine.update("Italian");
+  //   assertEquals(Cuisine.all().get(0).getType(), "Italian");
+  // }
+  //
+  // @Test
+  // public void delete_deletesCuisineTypeFromDB() {
+  //   Cuisine myCuisine = new Cuisine("American");
+  //   myCuisine.save();
+  //   myCuisine.delete();
+  //   assertEquals(Cuisine.all().size(), 0);
+  // }
 }
