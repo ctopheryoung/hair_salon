8b22831ae0be6d9739f688cef6b67fcdfa86ae69
Chris Young <ctopheryoung@gmail.com>
2016-02-29 23:43:59 -0800

    add more integration tests and pass:

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -29,9 +29,58 @@ public class AppTest extends FluentTest {
 
   @Test
   public void stylistIsDisplayedTest() {
-    Stylist testStylist = new Stylist ("Larry");
+    Stylist testStylist = new Stylist("Larry");
     testStylist.save();
     goTo("http://localhost:4567/stylists");
     assertThat(pageSource()).contains("Larry");
+  }
+
+  @Test
+    public void clientIsDisplayedTest() {
+    Client testClient = new Client("Donald Trump", "123-456-7890", 2);
+    testClient.save();
+    goTo("http://localhost:4567/client/all");
+    assertThat(pageSource()).contains("Donald Trump");
+  }
+
+  @Test
+  public void clientPhoneFormIsDisplayed() {
+    Stylist testStylist = new Stylist("Larry");
+    testStylist.save();
+
+    Client testClient = new Client("Donald Trump", "111-111-1111", testStylist.getId());
+    testClient.save();
+
+    String clientDisplay = String.format("http://localhost:4567/clients/%d", testClient.getId());
+    goTo(clientDisplay);
+    assertThat(pageSource()).contains("111-111-1111");
+  }
+
+  @Test
+  public void clientIsDeletedAndSuccessPageShows() {
+    Stylist testStylist = new Stylist("Larry");
+    testStylist.save();
+
+    Client testClient = new Client("Donald Trump", "111-111-1111", testStylist.getId());
+    testClient.save();
+
+    String clientPage = String.format("http://localhost:4567/clients/%d", testClient.getId());
+    goTo(clientPage);
+    click("button", withText("Delete Client"));
+    assertThat(pageSource()).contains("Client has succesfully been deleted.");
+  }
+
+  @Test
+  public void allClientsDisplayAllClientNamesOnStylistage() {
+    Stylist testStylist = new Stylist("Larry");
+    testStylist.save();
+    Client firstClient = new Client("Donald Trump", "123-456-7890", testStylist.getId());
+    firstClient.save();
+    Client secondClient = new Client("Hillary Clinton", "111-111-1111", testStylist.getId());
+    secondClient.save();
+    String categoryPath = String.format("http://localhost:4567/stylists/%d", testStylist.getId());
+    goTo(categoryPath);
+    assertThat(pageSource()).contains("Donald Trump");
+    assertThat(pageSource()).contains("Hillary Clinton");
   }
 }
